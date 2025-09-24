062ddbbacaf446b5dc26676b887017cb49b48d17
MaxKiselevqqe <150704735+MaxKiselevqqe@users.noreply.github.com>
2025-09-24 18:16:39 +0300

    Add files via upload

---
+++ praktPHP/prakt.php
@@ -0,0 +1,11 @@
+
+<?php
+$n = $_POST["N"]
+$x(float)readline("Введите значение x:");
+function calc($x)
+$sum = 0;
+for ($i= 3; $i <=x; $i += 2){
+    $sum += $i;
+}
+echo "Сумма S = ". $sum .;
+?>
