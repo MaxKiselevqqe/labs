811ed1a49f162697a11ff088bd458755f9815c3d
MaxKiselevqqe <150704735+MaxKiselevqqe@users.noreply.github.com>
2025-09-27 13:35:12 +0300

    Update prakt.php

--- praktPHP/prakt.php
+++ praktPHP/prakt.php
@@ -3,9 +3,18 @@
 $n = $_POST["N"]
 $x(float)readline("Введите значение x:");
 function calc($x)
-$sum = 0;
+$sum_for = 0;
 for ($i= 3; $i <=x; $i += 2){
-    $sum += $i;
+    $sum_for += $i;
 }
-echo "Сумма S = ". $sum .;
-?>
+$sum_while =0;
+$j = 3;
+for($j <= $x) {
+    $sum_Wwhile += $j:
+    $j +=2;
+    
+echo  $sum_for;
+    echo $sum_while;
+
+
+?>-
